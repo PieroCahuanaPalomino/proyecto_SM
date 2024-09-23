d09e221a2008a38fd2f62508c456332dea4604b5
PieroCahuanaPalomino <piero.cahuana.69.palomino@gmail.com>
2024-09-23 00:55:09 -0500

    Agregando orden en el ddl-auto yaml

--- ms-auth-service/src/main/java/com/proyect/library/MsAuthServiceApplication.java
+++ ms-auth-service/src/main/java/com/proyect/library/MsAuthServiceApplication.java
@@ -22,38 +22,5 @@ public class MsAuthServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MsAuthServiceApplication.class, args);
 	}
-	
-	@Autowired
-	AuthUserRepository userRepository;
-	
-	@Autowired
-	PasswordEncoder passwordEncoder;
-	
-	@Bean
-	CommandLineRunner init() {
-		return args -> {
-			UserEntity userEntity = UserEntity.builder()
-					.email("santiago.mendez@unmsm.edu.pe")
-					.username("Santiago.mendez")
-					.password(passwordEncoder.encode("77810931"))
-					.roles(Set.of(RoleEntity.builder().name(ERole.valueOf(ERole.ADMIN.name())).build())).build();
-
-			UserEntity userEntity2 = UserEntity.builder()
-					.email("liza.vidal@unmsm.edu.pe")
-					.username("Liza.vidal")
-					.password(passwordEncoder.encode("77810931"))
-					.roles(Set.of(RoleEntity.builder().name(ERole.valueOf(ERole.USER.name())).build())).build();
-
-			UserEntity userEntity3 = UserEntity.builder()
-					.email("marcos.herrera@unmsm.edu.pe")
-					.username("Marcos.herrera")
-					.password(passwordEncoder.encode("77810931"))
-					.roles(Set.of(RoleEntity.builder().name(ERole.valueOf(ERole.INVITED.name())).build())).build();
-
-			userRepository.save(userEntity);
-			userRepository.save(userEntity2);
-			userRepository.save(userEntity3);
-		};
-	}
 
 }
--- user-service/src/main/java/com/proyect/library/UserServiceApplication.java
+++ user-service/src/main/java/com/proyect/library/UserServiceApplication.java
@@ -24,4 +24,37 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Bean
+	CommandLineRunner init() {
+		return args -> {
+			UserEntity userEntity = UserEntity.builder()
+					.email("santiago.mendez@unmsm.edu.pe")
+					.username("Santiago.mendez")
+					.password(passwordEncoder.encode("77810931"))
+					.roles(Set.of(RoleEntity.builder().name(ERole.valueOf(ERole.ADMIN.name())).build())).build();
+
+			UserEntity userEntity2 = UserEntity.builder()
+					.email("liza.vidal@unmsm.edu.pe")
+					.username("Liza.vidal")
+					.password(passwordEncoder.encode("77810931"))
+					.roles(Set.of(RoleEntity.builder().name(ERole.valueOf(ERole.USER.name())).build())).build();
+
+			UserEntity userEntity3 = UserEntity.builder()
+					.email("marcos.herrera@unmsm.edu.pe")
+					.username("Marcos.herrera")
+					.password(passwordEncoder.encode("77810931"))
+					.roles(Set.of(RoleEntity.builder().name(ERole.valueOf(ERole.INVITED.name())).build())).build();
+
+			userRepository.save(userEntity);
+			userRepository.save(userEntity2);
+			userRepository.save(userEntity3);
+		};
+	}
 }
